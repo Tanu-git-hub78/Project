e7f0493534f612b11af13c17cb84d5dffa5262cd
Tanu-git-hub78 <165461033+Tanu-git-hub78@users.noreply.github.com>
2025-09-27 18:26:19 +0530

    Updated VivaEvaluation.tsx

---
+++ src/pages/examiner/VivaEvaluation.tsx
@@ -0,0 +1,13 @@
+// src/pages/examiner/VivaEvaluation.tsx
+import React from "react";
+
+const VivaEvaluation: React.FC = () => {
+  return (
+    <div>
+      <h1>Viva Evaluation</h1>
+      <p>This is the Viva Evaluation page for examiners.</p>
+    </div>
+  );
+};
+
+export default VivaEvaluation;
