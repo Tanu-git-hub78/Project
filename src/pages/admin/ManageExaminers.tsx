988fae18f398b49b5bf8c96a70a6bd9716c3a092
Tanu-git-hub78 <165461033+Tanu-git-hub78@users.noreply.github.com>
2025-09-27 18:01:43 +0530

    Updated ManageExaminers.tsx

---
+++ src/pages/admin/ManageExaminers.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ManageExaminers: React.FC = () => {
+  return (
+    <div>
+      <h1>Manage Examiners Page</h1>
+      <p>This is a placeholder for the Manage Examiners component.</p>
+    </div>
+  );
+};
+
+export default ManageExaminers;
