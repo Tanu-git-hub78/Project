62da6443eafaf171653a61af47970197c3371179
Tanu-git-hub78 <165461033+Tanu-git-hub78@users.noreply.github.com>
2025-09-27 18:28:58 +0530

    Updated ExaminerProfile.tsx

---
+++ src/pages/examiner/ExaminerProfile.tsx
@@ -0,0 +1,13 @@
+// src/pages/examiner/ExaminerProfile.tsx
+import React from "react";
+
+const ExaminerProfile: React.FC = () => {
+  return (
+    <div>
+      <h1>Examiner Profile</h1>
+      <p>This is the profile page for examiners. You can add profile details here.</p>
+    </div>
+  );
+};
+
+export default ExaminerProfile;
