4a709360050700490457671a0326052c9b370aa2
Tanu-git-hub78 <165461033+Tanu-git-hub78@users.noreply.github.com>
2025-09-27 18:23:53 +0530

    Updated AssignedStudents.tsx

---
+++ src/pages/examiner/AssignedStudents.tsx
@@ -0,0 +1,13 @@
+// src/pages/examiner/AssignedStudents.tsx
+import React from "react";
+
+const AssignedStudents: React.FC = () => {
+  return (
+    <div>
+      <h1>Assigned Students</h1>
+      <p>This is the Assigned Students page for examiners.</p>
+    </div>
+  );
+};
+
+export default AssignedStudents;
