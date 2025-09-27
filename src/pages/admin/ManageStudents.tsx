25ce72fb0d2fc76a319dded3c9f1ecb4670dd631
Tanu-git-hub78 <165461033+Tanu-git-hub78@users.noreply.github.com>
2025-09-27 17:58:35 +0530

    Updated ManageStudents.tsx

---
+++ src/pages/admin/ManageStudents.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ManageStudents: React.FC = () => {
+  return (
+    <div>
+      <h1>Manage Students Page</h1>
+      <p>This is a placeholder for the Manage Students component.</p>
+    </div>
+  );
+};
+
+export default ManageStudents;
