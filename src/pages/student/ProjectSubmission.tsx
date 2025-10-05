db83fec07f80554288145b332c98ccf7368be8c7
Tanu-git-hub78 <165461033+Tanu-git-hub78@users.noreply.github.com>
2025-10-05 21:10:28 +0530

    Fix CircleCheck import error

--- src/pages/student/ProjectSubmission.tsx
+++ src/pages/student/ProjectSubmission.tsx
@@ -99,7 +99,7 @@ const ProjectSubmission: React.FC = () => {
 
           <div className="bg-blue-50 border border-blue-200 rounded-lg p-4 mb-8">
             <div className="flex items-center">
-              <CircleCheck className="h-5 w-5 text-blue-600 mr-3" />
+              <CheckCircle className="h-5 w-5 text-blue-600 mr-3" />
               <span className="text-blue-800 font-medium">Ready to Submit - Upload your project file and submit</span>
             </div>
           </div>
